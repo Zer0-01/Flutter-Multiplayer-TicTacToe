5bc0b22d79aeaabbd1383c20d863bd97992b5340
Anas Zulkifli Mohd Jeffry <anas.zulkifli2001@gmail.com>
2024-05-13 00:26:03 +0800

    connecting to mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 //importing modules
+const e = require('express');
 const express = require('express');
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,6 +14,19 @@ var io = require('socket.io')(server);
 //middleware
 app.use(express.json());
 
+var username = encodeURIComponent("Zer0-01");
+var password = encodeURIComponent("Blackkuro01#");
+
+const DB = `mongodb+srv://${username}:${password}@cluster0.0frs88j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log("Connection successful");
+    }).catch((e) => {
+        console.log(e);
+    });
+
 server.listen(port, '0.0.0.0', () => {
-    console.log(`Server started and runnin on port ${port}`);
+    console.log(`Server started and running on port ${port}`);
 });
